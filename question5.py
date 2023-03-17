914e86c78f341644be3c6585ffe2e7f40d57168a
Vivek <84849703+vt57299@users.noreply.github.com>
2023-03-17 17:09:32 +0530

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,1 +1,12 @@
-# Write a program to sort list of strings (similar to that of dictionary)
+l = ["apple", "banana", "orange", "grape"]              
+sorted_list = [''.join(sorted(i)) for i in l]
+print("1: ",sorted_list)
+
+
+l.sort()
+print("2: ",l)
+
+for i in l:
+    print("3: ",i)
+
+print("4: ",sorted(l))
