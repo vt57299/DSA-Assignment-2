ac314a7728a19cc123494aa075c4056168fd1858
Vivek <84849703+vt57299@users.noreply.github.com>
2023-03-17 16:23:19 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,1 +1,13 @@
 # Implement inserstion sort
+def insertionsort(arr):
+    for index in range(1,len(arr)):
+        curr_element = arr[index]
+        pos = index
+        while curr_element < arr[pos-1] and pos>0:
+            arr[pos] = arr[pos-1]
+            pos = pos-1
+        arr[pos] = curr_element
+
+l = [2,4,5,653,2,3345,7,3,23]
+insertionsort(l)
+print(l)
